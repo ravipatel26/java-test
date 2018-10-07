3917057ac1aa7bb212ae706dd2e577e38d227c97
ravipatel26 <ravikumarpatel2610@gmail.com>
2018-10-06 23:23:39 -0400

    Refactoring User class

--- src/main/java/com/h2rd/refactoring/usermanagement/User.java
+++ src/main/java/com/h2rd/refactoring/usermanagement/User.java
@@ -6,25 +6,36 @@ import java.util.List;
 @XmlRootElement
 public class User {
 
-    String name;
-    String email;
-    List<String> roles;
+    private String name;
+    private String email;
+    private List<String> roles;
+    
+    public User(String name, String email) {
+        this.roles = new ArrayList<String>();
+        this.name = name;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public String getEmail() {
         return email;
     }
+    
     public void setEmail(String email) {
         this.email = email;
     }
+    
     public List<String> getRoles() {
         return roles;
     }
+    
     public void setRoles(List<String> roles) {
         this.roles = roles;
     }
